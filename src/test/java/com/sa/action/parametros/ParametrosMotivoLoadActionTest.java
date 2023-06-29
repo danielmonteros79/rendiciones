cdc2993f4bbb32884b62e7cae80852e80e9bb216
ezequiellautaro.huaytaromay.contractor.gdevtools.com <ezequiellautaro.huaytaromay.contractor@gdevtools.com>
2023-06-29 15:29:18 -0300

    Adicion de tests unitarios para ParametrosMotivoLoadAction

---
+++ src/test/java/com/sa/action/parametros/ParametrosMotivoLoadActionTest.java
@@ -0,0 +1,12 @@
+package com.sa.action.parametros;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParametrosMotivoLoadActionTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+}
