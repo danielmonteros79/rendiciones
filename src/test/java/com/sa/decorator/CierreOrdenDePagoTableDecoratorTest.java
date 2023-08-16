1197ab2d2234af8a96c01b66d4dcf2256657bd73
Mateo Valdesolo <mateoluis.valdesolo.contractor@gdevtools.com>
2023-08-16 16:33:37 -0300

    Termino tests unitarios de la clase CierreOrdenDePagoTableDecorator

--- src/test/java/com/sa/decorator/CierreOrdenDePagoTableDecoratorTest.java
+++ src/test/java/com/sa/decorator/CierreOrdenDePagoTableDecoratorTest.java
@@ -2,48 +2,129 @@ package com.sa.decorator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
 
+import com.sa.entities.Rendicion;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.PageContext;
 
 class CierreOrdenDePagoTableDecoratorTest {
 
+    @Mock
+    PageContext pageContext;
+
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    HttpSession session;
+
+    @Mock
+    ServletContext servletContext;
+
+    @InjectMocks
+    CierreOrdenDePagoTableDecorator cierreOrdenDePagoTableDecorator;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    @Disabled("Revisar NullPointerException linea20")
+    @DisplayName("Testeando getEditarLink")
     void testGetEditarLink() {
+        Rendicion rendicion = new Rendicion();
+        rendicion.setId(1);
+        rendicion.setUsuarioRendicion("usuarioRendicion");
 
-        (new CierreOrdenDePagoTableDecorator()).getEditarLink();
+        when(pageContext.getRequest()).thenReturn(request);
+        when(request.getContextPath()).thenReturn("contextPath");
+
+        cierreOrdenDePagoTableDecorator.init(pageContext, null, null);
+        cierreOrdenDePagoTableDecorator.initRow(rendicion, 0, 0);
+
+        String result = cierreOrdenDePagoTableDecorator.getEditarLink();
+
+        assertEquals("<a href=\"contextPath/cierreDetalle.do?codigo=1&usuarioRend=usuarioRendicion\"><i class=\"bbva-icon icon-coronita_search\" data-toggle=\"tooltip\" title=\"Ver\"></i></a>",result);
     }
 
 
     @Test
-    @Disabled("Revisar NullPointerException linea33")
+    @DisplayName("Testeando getThubanLink")
     void testGetThubanLink() {
+        Rendicion rendicion = new Rendicion();
+        rendicion.setId(1);
 
-        (new CierreOrdenDePagoTableDecorator()).getThubanLink();
+        when(pageContext.getRequest()).thenReturn(request);
+        when(request.getSession()).thenReturn(session);
+        when(session.getServletContext()).thenReturn(servletContext);
+        when(servletContext.getAttribute("rendicion.link.thuban")).thenReturn("linkThuban");
+
+        cierreOrdenDePagoTableDecorator.init(pageContext, null, null);
+        cierreOrdenDePagoTableDecorator.initRow(rendicion, 0, 0);
+
+        String result = cierreOrdenDePagoTableDecorator.getThubanLink();
+
+        assertEquals("<a href=\"#a\" class=\"text-gray\" onclick=\"showThuban('linkThuban1')\"><i class=\"bbva-icon icon-uniE0D2 fa-lg\" data-toggle=\"tooltip\" title=\"Thuban\"></i></a>",result);
     }
 
     @Test
-    @Disabled("Revisar NullPointerException linea 47")
+    @DisplayName("Testeando getJournalLink")
     void testGetJournalLink() {
+        Rendicion rendicion = new Rendicion();
+        rendicion.setId(1);
 
-        (new CierreOrdenDePagoTableDecorator()).getJournalLink();
+        cierreOrdenDePagoTableDecorator.init(pageContext, null, null);
+        cierreOrdenDePagoTableDecorator.initRow(rendicion, 0, 0);
+
+        String result = cierreOrdenDePagoTableDecorator.getJournalLink();
+
+        assertEquals("<a href=\"#a\" class=\"text-gray\" onclick=\"modalJournalShow('1')\"><i class=\"bbva-icon icon-coronita_bookstore fa-lg\" data-toggle=\"tooltip\" title=\"Journal\"></i></a>",result);
     }
 
     @Test
-    @Disabled("Revisar NullPointerException linea 20 y 54")
+    @DisplayName("Testeando getOpciones")
     void testGetOpciones() {
+        Rendicion rendicion = new Rendicion();
+        rendicion.setId(1);
+        rendicion.setUsuarioRendicion("usuarioRendicion");
 
+        when(pageContext.getRequest()).thenReturn(request);
+        when(request.getContextPath()).thenReturn("contextPath");
+        when(request.getSession()).thenReturn(session);
+        when(session.getServletContext()).thenReturn(servletContext);
+        when(servletContext.getAttribute("rendicion.link.thuban")).thenReturn("linkThuban");
 
-        (new CierreOrdenDePagoTableDecorator()).getOpciones();
+        cierreOrdenDePagoTableDecorator.init(pageContext, null, null);
+        cierreOrdenDePagoTableDecorator.initRow(rendicion, 0, 0);
+
+        String result = cierreOrdenDePagoTableDecorator.getOpciones();
+
+        assertEquals("<a href=\"contextPath/cierreDetalle.do?codigo=1&usuarioRend=usuarioRendicion\"><i class=\"bbva-icon icon-coronita_search\" data-toggle=\"tooltip\" title=\"Ver\"></i></a>&nbsp;<a href=\"#a\" class=\"text-gray\" onclick=\"showThuban('linkThuban1')\"><i class=\"bbva-icon icon-uniE0D2 fa-lg\" data-toggle=\"tooltip\" title=\"Thuban\"></i></a>&nbsp;<a href=\"#a\" class=\"text-gray\" onclick=\"modalJournalShow('1')\"><i class=\"bbva-icon icon-coronita_bookstore fa-lg\" data-toggle=\"tooltip\" title=\"Journal\"></i></a>",result);
     }
 
     @Test
-    @Disabled("Revisar NullPointerException linea 62")
+    @DisplayName("Testeando getCheck")
     void testGetCheck() {
+        Rendicion rendicion = new Rendicion();
+        rendicion.setId(1);
 
+        cierreOrdenDePagoTableDecorator.init(pageContext, null, null);
+        cierreOrdenDePagoTableDecorator.initRow(rendicion, 0, 0);
 
-        (new CierreOrdenDePagoTableDecorator()).getCheck();
+        String result = cierreOrdenDePagoTableDecorator.getCheck();
+
+        assertEquals("<input type='checkbox' value='1' onclick=\"clickCheckbox(1, this)\">",result);
     }
 
     @Test
