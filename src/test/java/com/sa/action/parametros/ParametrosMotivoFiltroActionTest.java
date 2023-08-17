8c32b7ca896e6b4b2060c06db082955986d63383
ezequiellautaro.huaytaromay.contractor.gdevtools.com <ezequiellautaro.huaytaromay.contractor@gdevtools.com>
2023-08-17 11:19:09 -0300

    Correccion de tests unitarios en ParametrosMotivoFiltroAction

--- src/test/java/com/sa/action/parametros/ParametrosMotivoFiltroActionTest.java
+++ src/test/java/com/sa/action/parametros/ParametrosMotivoFiltroActionTest.java
@@ -3,103 +3,99 @@ package com.sa.action.parametros;
 import ar.com.bbva.web.impl.SAMWebApplication;
 import ar.com.bbva.web.impl.SAMWebClient;
 import com.sa.entities.Usuario;
-import com.sa.entities.parametros.ParametroMotivo;
-import com.sa.form.parametros.ParametrosMotivoFiltroForm;
 import com.sa.services.ParametrosService;
+import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.mock.MockHttpServletRequest;
-import org.apache.struts.mock.MockHttpSession;
-import org.apache.struts.mock.MockServletContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.*;
 
-import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 class ParametrosMotivoFiltroActionTest {
 
   @Mock
-  HttpServletResponse httpServletResponse;
-
+  PrintWriter printWriterMocked;
+  @Mock
+  ActionMapping actionMappingMocked;
+  @Mock
+  ActionForward actionForwardMocked;
+  @Mock
+  ActionForm actionFormMocked;
+  @Mock
+  SAMWebClient samWebClientMocked;
+  @Mock
+  SAMWebApplication samWebApplicationMocked;
+  @Mock
+  HttpServletRequest httpServletRequestMocked;
+  @Mock
+  HttpServletResponse httpServletResponseMocked;
   @InjectMocks
   ParametrosMotivoFiltroAction parametrosMotivoFiltroAction;
 
   public static Stream<Arguments> executeAction() {
     //given
-    ActionMapping actionMapping = new ActionMapping();
-    SAMWebApplication samWebApplication = new SAMWebApplication();
-    HttpSession httpSession = new MockHttpSession();
-    SAMWebClient samWebClient = new SAMWebClient();
-    MockHttpServletRequest request = new MockHttpServletRequest();
-    List<Usuario> delegados = new ArrayList<>();
-    ServletContext servletContext = new MockServletContext();
-
-    ParametrosMotivoFiltroForm parametrosMotivoFiltroForm = new ParametrosMotivoFiltroForm();
-    parametrosMotivoFiltroForm.setCodigo("11");
-
-    ParametroMotivo parametroMotivo = new ParametroMotivo();
-
-    List<ParametroMotivo> parametroMotivoList = new ArrayList<>();
-    parametroMotivoList.add(parametroMotivo);
-
-    Usuario usuario2 = new Usuario("55", "2", "", 77, "2c", new ArrayList<>());
-    Usuario usuario3 = new Usuario("55", "2", "", 77, "2c", new ArrayList<>());
-    delegados.add(usuario2);
-    delegados.add(usuario3);
-    Usuario usuario = new Usuario("55", "2", "Luis Machado", 77, "2c", delegados);
-
-    httpSession.setAttribute("usuario", usuario);
-
-    request.setHttpSession(httpSession);
-
-    actionMapping.addForwardConfig(new ActionForward("success", "path1", false));
-
-    samWebClient.setSession(httpSession);
-    samWebClient.setLoginOk(true);
-    samWebClient.setId("55");
-    samWebClient.setAttribute("usuario", usuario);
-
-    samWebApplication.setContext(servletContext);
-    samWebApplication.setClientClass("");
-    samWebApplication.setAttribute("usuario", usuario);
+    String action = "filtrar";
 
     return Stream.of(
-        Arguments.of(actionMapping, samWebApplication, samWebClient, request,parametrosMotivoFiltroForm, usuario, parametroMotivoList)
+        Arguments.of(""),
+        Arguments.of(action)
                     );
   }
 
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
+    Usuario usuario = new Usuario("", "", "", 0, "", new ArrayList<>());
+    parametrosMotivoFiltroAction.setSessionUserWorking(usuario);
   }
 
   @ParameterizedTest
   @MethodSource("executeAction")
   @DisplayName("Should determine what action execute")
-  void shouldDetermineWhatActionExecute(ActionMapping actionMapping, SAMWebApplication samApplication, SAMWebClient samClient, MockHttpServletRequest request,
-                                        ParametrosMotivoFiltroForm parametrosMotivoFiltroForm, Usuario usuario, List<ParametroMotivo> parametroMotivoList) throws Exception {
+  void shouldDetermineWhatActionExecute(String action) throws Exception {
     //when
+    when(httpServletRequestMocked.getParameter("action")).thenReturn(action);
+
+    when(actionMappingMocked.findForward(anyString())).thenReturn(actionForwardMocked);
+    when(httpServletResponseMocked.getWriter()).thenReturn(printWriterMocked);
+
     try (MockedConstruction<ParametrosService> parametrosServiceMC = Mockito.mockConstruction(ParametrosService.class,
         (mockParametrosService, context) -> {
-          when(mockParametrosService.getMotivos(parametrosMotivoFiltroForm.getCodigo(), usuario.getIdUser())).thenReturn(parametroMotivoList);
+          when(mockParametrosService.getMotivos(anyString(), anyString())).thenReturn(new ArrayList<>());
         })) {
       //then
-      ActionForward actionForwardToAssert = parametrosMotivoFiltroAction.executeAction(actionMapping, parametrosMotivoFiltroForm, samApplication, samClient, request,
-          httpServletResponse);
+      ActionForward actionForwardToAssert = parametrosMotivoFiltroAction.executeAction(actionMappingMocked, actionFormMocked, samWebApplicationMocked,
+          samWebClientMocked, httpServletRequestMocked, httpServletResponseMocked);
       assertNotNull(actionForwardToAssert);
     }
+  }
+
+  @Test
+  @DisplayName("Should catch an exception")
+  void shouldCatchAnException() throws Exception {
+    //when
+    when(httpServletRequestMocked.getParameter("action")).thenReturn("filtrar");
+
+    when(actionMappingMocked.findForward(anyString())).thenReturn(actionForwardMocked);
+    when(httpServletResponseMocked.getWriter()).thenReturn(printWriterMocked);
+    //then
+    ActionForward actionForwardToAssert = parametrosMotivoFiltroAction.executeAction(actionMappingMocked, actionFormMocked, samWebApplicationMocked,
+        samWebClientMocked, httpServletRequestMocked, httpServletResponseMocked);
+    assertNull(actionForwardToAssert);
   }
 }
