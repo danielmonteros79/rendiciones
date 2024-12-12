f1d247d9ac946e62582298bc21fcb3b7d8902116
facundonahuel.cedolini.contractor@gdevtools.com <facundonahuel.cedolini.contractor@gdevtools.com>
2024-12-12 15:36:32 -0300

    Agregado cierre de rendición.

--- src/main/webapp/static/js/cierre/cierreDetalle.js
+++ src/main/webapp/static/js/cierre/cierreDetalle.js
@@ -25,3 +25,78 @@ function openImagenes() {
 	//modalImagenesShow($('#idRendicion').html(), true, $('#urlThuban').val());
 	window.open($('#urlThuban').val(), '_blank');
 }
+
+function generarCierre() {
+   $('#messageContainer').addClass('d-none');
+   $('#gastosTableMessage').addClass('d-none');
+
+   paramsOrdenDePago = {
+       action: 'generar',
+       idRendiciones: JSON.stringify([$('#idRendicion').html()])
+   };
+
+   showConfirm('generarCierreConfirm', '&iquest;Est&aacute;s seguro que quer&eacute;s generar el cierre de esta rendici&oacute;n?');
+}
+
+function generarCierreConfirm() {
+   callAjax('cierreOrdenDePago.do', paramsOrdenDePago, 'generarCierreConfirmSuccess', 'generarCierreConfirmError');
+}
+
+function generarCierreConfirmSuccess(data) {
+   showMessage2('#gastosTableMessage', data.message);
+   $('#modalConfirm').modal('hide');
+
+}
+
+function generarCierreConfirmError(data) {
+   showMessage2('#gastosTableMessage', data.error);
+   $('#modalConfirm').modal('hide');
+}
+
+function suspenderCierre() {
+    $('#messageContainer').addClass('d-none');
+    $('#gastosTableMessage').addClass('d-none');
+
+    var idRendicion = $('#idRendicion').html();
+
+    if (idRendicion) {
+        modalSuspenderCierreShow(JSON.stringify([idRendicion]));
+    } else {
+        showMessage2('#gastosTableMessage', 'No se pudo identificar la rendición.', 'error');
+        scrollToElem('#gastosTableMessage', true);
+    }
+}
+
+function suspenderCierreConfirm() {
+    var paramsSuspender = {
+        action: 'suspender',
+        idRendiciones: JSON.stringify([$('#idRendicion').html()])
+    };
+
+    callAjax('cierreDetalle.do', paramsSuspender, 'suspenderCierreConfirmSuccess', 'suspenderCierreConfirmError');
+}
+
+function suspenderCierreConfirmSuccess(data) {
+    showMessage2('#cierreOrdenDePagoTableMessage', data.message);
+    $('#modalConfirm').modal('hide');
+
+    console.log("Cierre suspendido exitosamente.");
+}
+
+
+function suspenderCierreConfirmError(data) {
+    showMessage2('#gastosTableMessage', data.error);
+    $('#modalConfirm').modal('hide');
+}
+
+function loadCierreTable() {
+    const dtParams = {
+        action: 'getRendicionGastos',
+        idRendicion: $('#idRendicion').html(),
+        usuarioRend: $('#usuarioRend').val(),
+        codMotivo: $('#codMotivo').val(),
+        estadoRend: $('#estadoRend').val()
+    };
+
+    loadTable('#gastosDtContainer', dtLink, dtParams);
+}
