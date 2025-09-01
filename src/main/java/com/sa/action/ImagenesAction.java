1675848c41d10f1b967af465a0ca218a67a8ccff
martinignacio.cabreracazenave.contractor.gdevtools.com <martinignacio.cabreracazenave.contractor@gdevtools.com>
2025-09-01 07:23:39 -0300

    fix estado counter linked with generar button

--- src/main/java/com/sa/action/ImagenesAction.java
+++ src/main/java/com/sa/action/ImagenesAction.java
@@ -27,154 +27,164 @@ import org.apache.commons.text.StringEscapeUtils;
 
 public class ImagenesAction extends RestriccionTransaccionAction {
 
-	public ActionForward executeAction(ActionMapping mapping, ActionForm form, SAMWebApplication samApplication, SAMWebClient samClient,
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
-		try {
-			RendicionAvisoForm frm = (RendicionAvisoForm) form;
+    public ActionForward executeAction(ActionMapping mapping, ActionForm form, SAMWebApplication samApplication, SAMWebClient samClient,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            RendicionAvisoForm frm = (RendicionAvisoForm) form;
 
-			if ("inicializar".equals(frm.getAction()))
-				return this.inicializar(request, response, frm, samClient);
-			else if ("cargarArchivo".equals(frm.getAction())) {
-				return this.cargarArchivo(request,response, frm);}
-			else if ("borrarArchivo".equals(frm.getAction()))
-				return this.borrarArchivo(request, response, frm);
-			else if ("generar".equals(frm.getAction()))
-				return this.generar(frm, mapping, request, response, samClient);
-			else if("descargarImg".equals(frm.getAction())) {
-				return this.descargarImg(request, response, frm, samClient);
-			}
+            if ("inicializar".equals(frm.getAction()))
+                return this.inicializar(request, response, frm, samClient);
+            else if ("cargarArchivo".equals(frm.getAction())) {
+                return this.cargarArchivo(request,response, frm);}
+            else if ("borrarArchivo".equals(frm.getAction()))
+                return this.borrarArchivo(request, response, frm);
+            else if ("generar".equals(frm.getAction()))
+                return this.generar(frm, mapping, request, response, samClient);
+            else if("descargarImg".equals(frm.getAction())) {
+                return this.descargarImg(request, response, frm, samClient);
+            }
 
-			return null;
-		} catch (Exception e) {
-			log.error("", e);
-			return writeError(response, e);
-		}
-	}
+            return null;
+        } catch (Exception e) {
+            log.error("", e);
+            return writeError(response, e);
+        }
+    }
 
-	private ActionForward inicializar(HttpServletRequest request, HttpServletResponse response, RendicionAvisoForm frm, SAMWebClient samClient) throws Exception {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		ThubanService thubanService = new ThubanService(samClient);
-		String thubanUser = (String) request.getSession().getServletContext().getAttribute("esb.thuban.user");
-		String thubanPass = (String) request.getSession().getServletContext().getAttribute("esb.thuban.pass");
-		String thubanClaseDoc = (String) request.getSession().getServletContext().getAttribute("esb.thuban.clase.documental");
-		
-		frm.clean();
-		
-		String idRendicion = request.getParameter("idRend");
-		
-		List<Archivo> archivos = thubanService.buscarArchivos(thubanClaseDoc, thubanUser, thubanPass, idRendicion);
-		resp.put("archivos", archivos);
+    private ActionForward inicializar(HttpServletRequest request, HttpServletResponse response, RendicionAvisoForm frm, SAMWebClient samClient) throws Exception {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        ThubanService thubanService = new ThubanService(samClient);
+        String thubanUser = (String) request.getSession().getServletContext().getAttribute("esb.thuban.user");
+        String thubanPass = (String) request.getSession().getServletContext().getAttribute("esb.thuban.pass");
+        String thubanClaseDoc = (String) request.getSession().getServletContext().getAttribute("esb.thuban.clase.documental");
+        
+        frm.clean();
+        
+        String idRendicion = request.getParameter("idRend");
+        
+        List<Archivo> archivos = thubanService.buscarArchivos(thubanClaseDoc, thubanUser, thubanPass, idRendicion);
+        resp.put("archivos", archivos);
 
-		return writeJson(response, resp);
-	}
-	
-	private ActionForward descargarImg(HttpServletRequest request, HttpServletResponse response, RendicionAvisoForm frm, SAMWebClient samClient) throws Exception {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		ThubanService thubanService = new ThubanService(samClient);
-		String thubanUser = (String) request.getSession().getServletContext().getAttribute("esb.thuban.user");
-		String thubanPass = (String) request.getSession().getServletContext().getAttribute("esb.thuban.pass");
-		String thubanClaseDoc = (String) request.getSession().getServletContext().getAttribute("esb.thuban.clase.documental");
-		
-		frm.clean();
-		
-		String idImagen = request.getParameter("idImagen");
-		
-		List<Archivo> archivo = thubanService.descargarArchivo(thubanClaseDoc, thubanUser, thubanPass, idImagen);
-		
-		resp.put("archivo", archivo);
+        return writeJson(response, resp);
+    }
+    
+    private ActionForward descargarImg(HttpServletRequest request, HttpServletResponse response, RendicionAvisoForm frm, SAMWebClient samClient) throws Exception {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        ThubanService thubanService = new ThubanService(samClient);
+        String thubanUser = (String) request.getSession().getServletContext().getAttribute("esb.thuban.user");
+        String thubanPass = (String) request.getSession().getServletContext().getAttribute("esb.thuban.pass");
+        String thubanClaseDoc = (String) request.getSession().getServletContext().getAttribute("esb.thuban.clase.documental");
+        
+        frm.clean();
+        
+        String idImagen = request.getParameter("idImagen");
+        
+        List<Archivo> archivo = thubanService.descargarArchivo(thubanClaseDoc, thubanUser, thubanPass, idImagen);
+        
+        resp.put("archivo", archivo);
 
-		return writeJson(response, resp);
-	}
-	
-	
+        return writeJson(response, resp);
+    }
+    
+    
 
-	private ActionForward cargarArchivo(HttpServletRequest request,HttpServletResponse response, RendicionAvisoForm frm) throws Exception {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		
-		String extension = request.getParameter("tipoArchivo").toLowerCase();
-		String base64Decoded = request.getParameter("base64");
-	    //byte[] decodedData = DatatypeConverter.parseBase64Binary(base64Decoded);
+    private ActionForward cargarArchivo(HttpServletRequest request,HttpServletResponse response, RendicionAvisoForm frm) throws Exception {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        
+        String extension = request.getParameter("tipoArchivo").toLowerCase();
+        String base64Decoded = request.getParameter("base64");
+        //byte[] decodedData = DatatypeConverter.parseBase64Binary(base64Decoded);
      
-		if (!extension.contains("pdf") && !extension.contains("tif")) {
-		    String safeFileName = StringEscapeUtils.escapeHtml4(frm.getArchivo().getFileName());
-		    return writeError(response, safeFileName + ": El archivo no es un PDF válido.");
-		}
-		
-		String nombreArchivo = StringEscapeUtils.escapeHtml4(request.getParameter("nombreArchivo"));
-		resp.put("nombreArchivo", nombreArchivo);
+        if (!extension.contains("pdf") && !extension.contains("tif")) {
+            String safeFileName = StringEscapeUtils.escapeHtml4(frm.getArchivo().getFileName());
+            return writeError(response, safeFileName + ": El archivo no es un PDF válido.");
+        }
+        
+        String nombreArchivo = StringEscapeUtils.escapeHtml4(request.getParameter("nombreArchivo"));
+        resp.put("nombreArchivo", nombreArchivo);
 
-		Archivo archivo = new Archivo();
-		archivo.setNomArchivo(request.getParameter("nombreArchivo"));
-		archivo.setInputStream(frm.getArchivo().getInputStream());
-		archivo.setBase64File(base64Decoded);
-		
-		frm.getArchivosASubir().add(archivo);
+        Archivo archivo = new Archivo();
+        archivo.setNomArchivo(request.getParameter("nombreArchivo"));
+        archivo.setInputStream(frm.getArchivo().getInputStream());
+        archivo.setBase64File(base64Decoded);
+        
+        frm.getArchivosASubir().add(archivo);
 
-		return writeJson(response, resp);
+        return writeJson(response, resp);
 
-	}
+    }
 
 
-	private ActionForward borrarArchivo(HttpServletRequest request, HttpServletResponse response, RendicionAvisoForm frm) throws Exception {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		
-		resp.put("nombreArchivo", request.getParameter("nombreArchivo"));
-		
-		boolean encontro = false;
-		for (int i = 0; i < frm.getArchivosASubir().size() && !encontro; i++) {
-			Archivo archivo = frm.getArchivosASubir().get(i);
+    private ActionForward borrarArchivo(HttpServletRequest request, HttpServletResponse response, RendicionAvisoForm frm) throws Exception {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        
+        resp.put("nombreArchivo", request.getParameter("nombreArchivo"));
+        
+        boolean encontro = false;
+        for (int i = 0; i < frm.getArchivosASubir().size() && !encontro; i++) {
+            Archivo archivo = frm.getArchivosASubir().get(i);
 
-			if (archivo.getNomArchivo().equals(request.getParameter("nombreArchivo"))) {
-				encontro = true;
-				frm.getArchivosASubir().remove(i);
-			}
-		}
+            if (archivo.getNomArchivo().equals(request.getParameter("nombreArchivo"))) {
+                encontro = true;
+                frm.getArchivosASubir().remove(i);
+            }
+        }
 
-		return writeJson(response, resp);
-	}
+        return writeJson(response, resp);
+    }
 
-	private ActionForward generar(RendicionAvisoForm frm, ActionMapping mapping, HttpServletRequest request, HttpServletResponse response,
-			SAMWebClient samClient) throws Exception {
-		Map<String, Object> resp = new HashMap<String, Object>();
-		String thubanUser = (String) request.getSession().getServletContext().getAttribute("esb.thuban.user");
-		String thubanPass = (String) request.getSession().getServletContext().getAttribute("esb.thuban.pass");
-		String thubanClaseDoc = (String) request.getSession().getServletContext().getAttribute("esb.thuban.clase.documental");
-		
-		String message = "OK: ARCHIVOS SUBIDOS CON ÉXITO.";
-		String idRendicion = request.getParameter("idRendicion");
-	
-		String glg = request.getParameter("glg");
-		boolean esAprobacion = request.getParameter("esAprobacion").equals("true");
+    private ActionForward generar(RendicionAvisoForm frm, ActionMapping mapping, HttpServletRequest request, HttpServletResponse response,
+            SAMWebClient samClient) throws Exception {
+        Map<String, Object> resp = new HashMap<String, Object>();
+        String thubanUser = (String) request.getSession().getServletContext().getAttribute("esb.thuban.user");
+        String thubanPass = (String) request.getSession().getServletContext().getAttribute("esb.thuban.pass");
+        String thubanClaseDoc = (String) request.getSession().getServletContext().getAttribute("esb.thuban.clase.documental");
+        
+        String message = "OK: ARCHIVOS SUBIDOS CON ÉXITO.";
+        String idRendicion = request.getParameter("idRendicion");
+    
+        String glg = request.getParameter("glg");
+        boolean esAprobacion = request.getParameter("esAprobacion").equals("true");
 
-		RendicionesService rendicionesService = new RendicionesService(samClient);
-		AprobacionesService aprobacionesService = new AprobacionesService(samClient);
-		ThubanService thubanService = new ThubanService(samClient);
-		
-		Rendicion rendicion = null;
-		if (esAprobacion)
-			rendicion = aprobacionesService.getAprobacionesPendientes(idRendicion, null, null, glg, this.getSessionUserWorking().getIdUser()).get(0);
-		else
-			rendicion = rendicionesService.obtenerListadoRendiciones(this.getSessionUserWorking().getIdUser(), idRendicion, null, null, null).get(0);
-	
-		rendicion.setUsuarioRendicion(this.getSessionUserWorking().getIdUser());
-		rendicion.setId(Integer.parseInt(idRendicion));
-		rendicion.setCostosDestino(String.valueOf(this.getSessionUserWorking().getCcostos()));
+        RendicionesService rendicionesService = new RendicionesService(samClient);
+        AprobacionesService aprobacionesService = new AprobacionesService(samClient);
+        ThubanService thubanService = new ThubanService(samClient);
+        
+        Rendicion rendicion = null;
+        if (esAprobacion)
+            rendicion = aprobacionesService.getAprobacionesPendientes(idRendicion, null, null, glg, this.getSessionUserWorking().getIdUser()).get(0);
+        else
+            rendicion = rendicionesService.obtenerListadoRendiciones(this.getSessionUserWorking().getIdUser(), idRendicion, null, null, null).get(0);
+    
+        rendicion.setUsuarioRendicion(this.getSessionUserWorking().getIdUser());
+        rendicion.setId(Integer.parseInt(idRendicion));
+        rendicion.setCostosDestino(String.valueOf(this.getSessionUserWorking().getCcostos()));
 
-		List<String> errores = thubanService.publicarDocumentos(thubanClaseDoc, thubanUser, thubanPass, rendicion, frm.getArchivosASubir());
-		
-		if (errores.size() == frm.getArchivosASubir().size())
-			return writeError(response, StringUtils.join(errores.toArray(), "<br><br>"));
+        List<String> errores = thubanService.publicarDocumentos(thubanClaseDoc, thubanUser, thubanPass, rendicion, frm.getArchivosASubir());
+        
+        if (errores.size() == frm.getArchivosASubir().size())
+            return writeError(response, StringUtils.join(errores.toArray(), "<br><br>"));
 
-		
-		if (errores.size() > 0)
-			message += "<br><br>" + StringUtils.join(errores.toArray(), "<br>");
-		
-		resp.put("message", message);
+        
+        if (errores.size() > 0)
+            message += "<br><br>" + StringUtils.join(errores.toArray(), "<br>");
+        
 
-		return writeJson(response, resp);
-	
-	}
-	
-	
+        try {
+            rendicionesService.activaRechazaRendicion("ESCAN", this.getSessionUserWorking().getIdUser(), String.valueOf(rendicion.getId()));
+            if (rendicionesService.getMsg() != null)
+                message += "<br>" + rendicionesService.getMsg();
+        } catch (Exception ex) {
+            message += "<br>No se pudo actualizar el estado de la rendicion.";
+            log.error("Error actualizando estado a ESCAN luego de publicar documentos", ex);
+        }
+        
+        resp.put("message", message);
+
+        return writeJson(response, resp);
+    
+    }
+    
+    
 
 }
